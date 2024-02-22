8a0ade5c37e392aee07dc6a0124bb23fcd973303
parkerkotlarz <157265957+parkerkotlarz@users.noreply.github.com>
2024-02-21 23:26:06 -0500

    Add files via upload

---
+++ iso2mesh/fillsurf.m
@@ -0,0 +1,19 @@
+function [no,el]=fillsurf(node,face)
+%
+% [no,el]=fillsurf(node,face)
+%
+% calculate the enclosed volume for a closed surface
+%
+% author: Qianqian Fang, <q.fang at neu.edu>
+%
+% input:
+%    node:  node coordinates
+%    face:  surface triangle list
+%
+% output:
+%    vol:   total volume of the enclosed space
+%
+% -- this function is part of iso2mesh toolbox (http://iso2mesh.sf.net)
+%
+
+[no,el]=surf2mesh(node,face,[],[],1,1,[],[],0,'tetgen','-YY');
---
+++ iso2mesh/getoptkey.m
@@ -0,0 +1,21 @@
+function varargout=getoptkey(varargin)
+%
+% val=getoptkey(key,default,opt)
+%
+% query the value of a key from a structure or a list of key/value pairs
+%
+% author: Qianqian Fang, <q.fang at neu.edu>
+%
+% input:
+%   key: a string name for the target struct field name
+%   default: the default value of the key is not found
+%   opt: a struct object; the field names will be searched to match the 
+%        key input, opt can be a list of 'keyname'/value pairs
+%
+% output:
+%   val: val=opt.key if found, otherwise val=default
+%
+% -- this function is part of iso2mesh toolbox (http://iso2mesh.sf.net)
+%
+
+[varargout{1:nargout}]=jsonopt(varargin{:});
